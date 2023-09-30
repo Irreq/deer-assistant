0d6890c947af9f39cbbf5285562f428f9b36eec1
Irreq <irreq@protonmail.com>
2023-09-30 17:51:50 +0200

    Added yay

--- prog/pm.py
+++ prog/pm.py
@@ -6,13 +6,23 @@ pattern = r"^\s*([^/\s]+)\/([^/\s]+)\s+(\S+)(?:\s+\[([^\]]+)\])?\s+(.*)"
 pattern = r"^(\S+)\/(\S+)\s+(\S+)(?:\s+\(([^)]+)\))?(?:\s+\(([^)]+)\))?\s+(.*)"
 
 pattern = r"^(\S+)\/(\S+)\s+(\S+)\s+(?:\(([^)]+)\))?\s+(?:\(([^)]+)\))?\s+(?:\(([^)]+)\))?\s+(.*)"
-import sys
+import sys, os
 
 import subprocess
 
-if len(sys.argv) != 2:
+print(sys.argv)
+
+if len(sys.argv) == 0:
     print("No name specified!")
     exit(1)
+
+if len(sys.argv[1:] == 2):
+    if sys.argv[2] == "info":
+        os.system("yay -Ss "+sys.argv[3])
+    else:
+        print("Invalid argument")
+    
+    exit()
 
 
 update_pattern = r"^(\S+)"
@@ -79,6 +89,8 @@ for item in form:
 #     print("Description:", description)
 #     print("Status:", status)
 #     print()
+
+final = []
 for match in matches:
     # print(match)
     # repository = match[0]
@@ -94,22 +106,29 @@ for match in matches:
     data = match[3]
     # is_installed = "installed" in match[4].lower()
     status = " "
+    status_static = " "
 
     if name in updates:
         status = placeholders["<BLUE BRIGHT>"] + "U"
+        status_static = "U"
     elif "installed" in match[4].lower():
         status = placeholders["<GREEN BRIGHT>"] + "I"
+        status_static = "I"
     elif "orphan" in match[4].lower():
         status = placeholders["<RED BRIGHT>"] + "R"
+        status_static = "R"
+    
     
     description = match[6]
 
-    item = (name, version, description, status)
+    item = (name, version, description, status, status_static)
 
     if repository in results:
         results[repository].append(item)
     else:
         results[repository] = [item]
+
+    final.append(item)
 
 
 def test():
@@ -129,8 +148,8 @@ count = 0
 for repo in results:
     out += placeholders["<YELLOW BRIGHT>"] + repo + placeholders["<RESET>"] + "/\n"
 
-    for (name, version, description, status) in results[repo]:
-        this = placeholders["<MAGENTA>"] + " " + str(count) + placeholders["<RESET>"] + " "
+    for (name, version, description, status,_) in results[repo]:
+        this = placeholders["<YELLOW>"] + " " + str(count) + placeholders["<RESET>"] + " "
         this += " "*(3 - len(str(count)))
         this += f"[{status+placeholders['<RESET>']}] "
 
@@ -152,14 +171,111 @@ for repo in results:
 
         
 
+def should_install():
+    while True:
+        choice = input(f'Install? [{placeholders["<GREEN BRIGHT>"]}Y{placeholders["<RESET>"]}/{placeholders["<RED BRIGHT>"]}n{placeholders["<RESET>"]}] ')
+        if choice.lower() == "n":
+            return False
+        elif choice.lower() == "y":
+            return True
+        
 
-print(out)
+def should(q):
+    while True:
+        choice = input(f'{q} [{placeholders["<GREEN BRIGHT>"]}Y{placeholders["<RESET>"]}/{placeholders["<RED BRIGHT>"]}n{placeholders["<RESET>"]}] ')
+        if choice.lower() == "n":
+            return False
+        elif choice.lower() == "y":
+            return True
+
+
+
+def what_do(n: int):
+    
+    (name, version, description, status, status_static) = final[n]
+    this = f"\n[{status+placeholders['<RESET>']}] "
+
+    this += name
+
+    this += " "*(length - len(name))
+
+    this += placeholders["<WHITE>"] + placeholders["<ITALIC>"] + description[:50] + placeholders["<RESET>"] + " "
+
+    this += placeholders["<BLUE BRIGHT>"] + version + placeholders["<RESET>"]
+
+    this += "\n"
+
+    print(this)
+
+    print()
+    print(subprocess.getoutput(f"yay -Si {names[n]}"))
+
+    if status_static == "U":
+        if should("Update?"):
+            print(f"Updating {name} -> {version}")
+            os.system(f"yay -Suy --noconfirm {name}")
+    elif status_static == "I":
+        if should("Remove?"):
+            print(f"Removing: {name}")
+            os.system(f"yay -Rns --noconfirm {name}")
+    elif status_static == "R":
+        if should("Remove?"):
+            print(f"Removing: {name}")
+            os.system(f"yay -Rns --noconfirm {name}")
+    else:
+        if should("Install?"):
+            print(f"Installing: {name}")
+            os.system(f"yay -Suy --noconfirm {name}")
+    
+
+
 
 num = 0
 
 
+try:
+    if count == 0:
+        print("No packages found :(")
+        exit()
+    elif count == 1:
+        # print(out)
+        what_do(0)
+        #print("Do you want to install?\n")
+        # choice = input(f'Install? [{placeholders["<GREEN BRIGHT>"]}Y{placeholders["<RESET>"]}/{placeholders["<RED BRIGHT>"]}n{placeholders["<RESET>"]}] ')
+    else:
+        print(out)
+
+        while True:
+
+            try:
+                choice = input(f"Pick a choice: 0-{count-1} ")
+            except:
+                print()
+                break
+            try:
+                n = int(choice)
+                assert 0<= n <= count
+                what_do(n)
+                break
+                print(subprocess.getoutput(f"yay -Si {names[n]}"))
+                if should_install():
+                    print("Installing")
+                    import os
+                    print(final[n])
+                    # os.system(f"yay -S --noconfirm {names[n]}")
+                    #print(subprocess.getoutput(f"yay -S --noconfirm {names[n]}"))
+                    break
+                else:
+                    break
+            except:
+                continue
+except:
+    print()
+
+exit()
+
 while True:
-    a = input()
+    a = input("")
 
     if a == "":
         # print("Empty")
